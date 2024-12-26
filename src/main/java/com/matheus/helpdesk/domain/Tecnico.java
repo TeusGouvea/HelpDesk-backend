ea5187e5e59bf4a4b72094db37081c42a3d934a3
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2024-12-26 20:33:45 -0300

    Atualizacao dos dados nas classes Chamado, Pessoa, CLiente e Tecnico.

--- src/main/java/com/matheus/helpdesk/domain/Tecnico.java
+++ src/main/java/com/matheus/helpdesk/domain/Tecnico.java
@@ -1,17 +1,27 @@
 package com.matheus.helpdesk.domain;
 
+import com.matheus.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
 
+@Entity
+public class Tecnico extends Pessoa{
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
