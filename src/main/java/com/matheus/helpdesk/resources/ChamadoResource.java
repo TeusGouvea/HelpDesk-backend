8aa5976183faa6f2678bae84d0d5e4b666c70ae8
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2025-01-06 17:06:40 -0300

    CRUD para chamados, clientes

---
+++ src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,50 @@
+package com.matheus.helpdesk.resources;
+
+import com.matheus.helpdesk.Services.ChamadoService;
+import com.matheus.helpdesk.domain.Chamado;
+import com.matheus.helpdesk.domain.dtos.ChamadoDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+        Chamado obj = service.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado obj = service.create(objDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id,@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado newObj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+    }
+
+}
